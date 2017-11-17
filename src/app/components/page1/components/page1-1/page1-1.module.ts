64f93928ef121036847e435aaa2753a180ef292d
Sergey Matvyenko <s.matvyenko@soft-industry.com>
2017-11-17 15:06:52 +0200

    remove trash files

--- src/app/components/page1/components/page1-1/page1-1.module.ts
+++ src/app/components/page1/components/page1-1/page1-1.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
 import { Page1_1Component } from './page1-1.component';
-import { PAGE1_1_ROUTES } from './page1-1.routes';
+
 
 @NgModule({
-    imports: [
-        CommonModule,
-            RouterModule.forChild (PAGE1_1_ROUTES),
+    imports: [        
     ],
     declarations: [
         Page1_1Component,
